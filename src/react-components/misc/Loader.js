6c7d06da0b20fbe92fc1efb7edc022b22592fb5d
Caspian Baska <caspianbaska@gmail.com>
2020-08-29 10:42:40 +1000

    rm dead asset

--- src/react-components/misc/Loader.js
+++ src/react-components/misc/Loader.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Loader.scss";
-import Spinger from "../../assets/spinger.gif"
 
 export function Loader({ message }) {
   return (
