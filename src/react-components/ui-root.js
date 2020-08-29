ee71cef49244e768678a8fdfc281f7ea97877224
Sarri Talhami <sarritalhami@gmail.com>
2020-08-29 13:47:40 +1000

    fix doofstick modal

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -151,7 +151,7 @@ const RoomAudioPlayer = React.forwardRef(
 
     if (track === null) return null;
 
-    console.info(`track: ${JSON.stringify(track)}`);
+    // console.info(`track: ${JSON.stringify(track)}`);
 
     const nextTrack = track => playlist[(playlist.indexOf(track) + 1) % playlist.length];
 
