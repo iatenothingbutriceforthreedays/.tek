5b8f51c997c56720e5cafd62acafb1611845d6a9
Huey Peard <hueypeard@users.noreply.github.com>
2020-08-29 21:57:58 +1000

    forgot position relative

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -3,8 +3,9 @@ import { SvgHoverButton, SvgToggleButton } from "../utils/svg-helpers";
 import { TextForm } from "./text-form";
 import { Slider } from "./slider";
 
-import DialogContainer from "../react-components/dialog-container";
 
+import DialogContainer from "../react-components/dialog-container";
+import Modal from "react-modal";
 import { SCHEMA } from "../storage/store";
 
 import { handleTextFieldBlur, handleTextFieldFocus } from "../utils/focus-utils";
@@ -35,6 +36,139 @@ import MicrophoneOff from "../assets/menu/MicrophoneOff.png";
 import MicrophoneOffHover from "../assets/menu/MicrophoneOff_Hover.png";
 import MicrophoneOn from "../assets/menu/MicrophoneOn.png";
 import MicrophoneOnHover from "../assets/menu/MicrophoneOn.png";
+
+const modalSmallBg = "https://str33m.dr33mphaz3r.net/static-assets/modal/modal-small-bg.png";
+const modalSmallBgWebp = "https://str33m.dr33mphaz3r.net/static-assets/modal/modal-small-bg.webp";
+const ExitButton = "https://str33m.dr33mphaz3r.net/static-assets/credits/ExitButton.png";
+const ExitButtonWebp = "https://str33m.dr33mphaz3r.net/static-assets/credits/ExitButton.webp";
+const ExitButtonHover = "https://str33m.dr33mphaz3r.net/static-assets/credits/ExitButtonHover.png";
+const ExitButtonHoverWebp = "https://str33m.dr33mphaz3r.net/static-assets/credits/ExitButtonHover.webp";
+
+const textStyles = {
+  background: "unset",
+  border: "1px solid #FFE6C1",
+  boxSizing: "border-box",
+  width: "280px",
+  color: "white",
+  padding: "8px",
+  filter: "drop-shadow(2px 2px 10px rgba(255, 184, 0, 0.94))",
+  borderRadius: "5px",
+  fontFamily: "Perpetua Titling MT",
+  fontStyle: "normal",
+  fontWeight: "300",
+  fontSize: "16px",
+  lineHeight: "18px",
+  textAlign: "center",
+  color: "#FFE6C1",
+};
+
+const HoverButton = ({ normalProps, hoverProps, style, href, ...otherProps }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  if(href) {
+    return (<a href={href}>
+      <img
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+        style={{
+          ...style,
+          cursor: "pointer"
+        }}
+        {...isShown ? hoverProps : normalProps}
+        src={isShown ? hoverProps['src'] : normalProps['src']}
+
+        {...otherProps}
+      />
+      </a>)
+  }
+
+  return (
+    <img
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+      style={{
+        ...style,
+        cursor: "pointer"
+      }}
+      {...isShown ? hoverProps : normalProps}
+      src={isShown ? hoverProps['src'] : normalProps['src']}
+      {...otherProps}
+    />
+  )
+};
+
+export const DoofLoadout = ({ isOpen, setInModal, name, onNameChange, doofstick, onDoofstickChange }) => {
+  return (<Modal
+    isOpen={isOpen}
+    onRequestClose={() => setInModal(false)}
+    contentLabel="3dit dr33m"
+    style={{
+      content: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      },
+    }}
+  >
+    <div style={{
+      backgroundImage: `url(${modalSmallBg})`,
+      position: 'relative',
+      width: "600px",
+      height: "600px",
+      backgroundPosition: "center",
+      backgroundRepeat: "none",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "contain",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column"
+    }}>
+      <HoverButton  onClick={(e) => {
+        e.preventDefault();
+        setInModal(false);
+        return false;
+      }} hoverProps={{ width: "32", height: "32", src: ExitButtonHover}} normalProps={{ width: "32", height: "32", src: ExitButton}}
+      
+      style={{
+        top: "44px",
+        left: "64px",
+        position: "absolute",
+        zIndex: "9001"
+      }}/>
+      <span>nam3</span>
+      <input
+        type="text"
+        style={textStyles}
+        value={name}
+        onChange={(e) => onNameChange(e.target.value)}
+        minLength={1}
+        maxLength={64}
+        spellCheck="false"
+        pattern={SCHEMA.definitions.profile.properties.displayName.pattern}
+        onFocus={e => handleTextFieldFocus(e.target)}
+        onBlur={() => handleTextFieldBlur()}
+      />
+      <span>D00F571C</span>
+      <textarea
+        id={"menu-doofstick-input"}
+        value={doofstick}
+        onChange={(e) => onDoofstickChange(e.target.value)}
+        minLength={0}
+        maxLength={120}
+        style={{
+          ...textStyles,
+          minHeight: '120px'
+        }}
+        spellCheck="false"
+        pattern={"^[A-Za-z0-9 -]{0,120}$"}
+        onFocus={e => handleTextFieldFocus(e.target)}
+        onBlur={() => handleTextFieldBlur()}
+      />
+    </div>
+  </Modal>);
+};
 
 export const Menu = ({
   watching,
@@ -193,34 +327,14 @@ export const Menu = ({
         </svg>
       </div>
 
-      {!hidden && inModal && (
-        <DialogContainer wide={true} title={"3dit dr33m"} onClose={() => setInModal(false)}>
-          <span>nam3</span>
-          <TextForm
-            id={"menu-name-input"}
-            value={name}
-            onValueChange={onNameChange}
-            minLength={1}
-            maxLength={64}
-            spellCheck="false"
-            pattern={SCHEMA.definitions.profile.properties.displayName.pattern}
-            onFocus={e => handleTextFieldFocus(e.target)}
-            onBlur={() => handleTextFieldBlur()}
-          />
-          <span>D00F571C</span>
-          <TextForm
-            id={"menu-doofstick-input"}
-            value={doofstick}
-            onValueChange={onDoofstickChange}
-            minLength={0}
-            maxLength={120}
-            spellCheck="false"
-            pattern={"^[A-Za-z0-9 -]{0,120}$"}
-            onFocus={e => handleTextFieldFocus(e.target)}
-            onBlur={() => handleTextFieldBlur()}
-          />
-        </DialogContainer>
-      )}
+      {!hidden && inModal && <DoofLoadout
+      isOpen={inModal}
+      setInModal={setInModal}
+      name={name}
+      onNameChange={onNameChange}
+      doofstick={doofstick}
+      onDoofstickChange={onDoofstickChange}
+      />}
     </>
   );
 };
