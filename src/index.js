faec02f7c9cf8573aadf50329894152f72b55a0d
Caspian Baska <caspianbaska@gmail.com>
2020-08-29 12:45:50 +1000

    add firebase logging ndat

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HomePage } from "./react-components/home/HomePage";
 import { lang, messages } from "./utils/i18n";
 import "./assets/stylesheets/globals.scss";
 import { AuthContextProvider } from "./react-components/auth/AuthContext";
-import {StripeProvider} from 'react-stripe-elements';
+import { StripeProvider } from "react-stripe-elements";
 
 registerTelemetry("/home", "Hubs Home Page");
 
@@ -19,14 +19,12 @@ function Root() {
   return (
     <IntlProvider locale={lang} messages={messages}>
       <AuthContextProvider store={store}>
-      <StripeProvider apiKey="pk_test_51Gyz7NBvCtr0PkoPQLGAdHP6qLtechXlKcTqgjLfcNgnCHmJuwYvG0IeP5yH4EUZJB5hzm5t3jsPWQKFn947Rgq100APsKrCXg">
-
-        <HomePage />
-    </StripeProvider>
+        <StripeProvider apiKey="pk_test_51Gyz7NBvCtr0PkoPQLGAdHP6qLtechXlKcTqgjLfcNgnCHmJuwYvG0IeP5yH4EUZJB5hzm5t3jsPWQKFn947Rgq100APsKrCXg">
+          <HomePage />
+        </StripeProvider>
       </AuthContextProvider>
     </IntlProvider>
   );
 }
-
 
 ReactDOM.render(<Root />, document.getElementById("home-root"));
