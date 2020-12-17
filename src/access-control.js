224a0b08bb4df9da9c22b9a6fbb951c29a591c60
Caspian Baska <caspianbaska@gmail.com>
2020-12-17 18:10:30 +1100

    check for window.freeMode when checking authorization

--- src/access-control.js
+++ src/access-control.js
@@ -3,7 +3,7 @@ import { getRoomMetadata, currentRoomKey } from "./room-metadata";
 // Returns true if redirect was performed
 export function redirectIfNotAuthorized(roomKey) {
   const meta = getRoomMetadata(roomKey); // uses current room if !roomKey
-  if (meta['requireLogin'] == false || (window.APP.hubChannel && window.APP.hubChannel.signedIn)) {
+  if (window.freeMode || meta['requireLogin'] == false || (window.APP.hubChannel && window.APP.hubChannel.signedIn)) {
     return false;
   } else {
     // TODO: Caspian -> put sign in ref here
